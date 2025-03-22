5add75600004e123b6bafc09d195e95f645ccf40
Elliott Amrouche <elliottamrouche27@gmail.com>
2025-03-22 23:36:46 +0100

    bdd fonctionnel

---
+++ junia_tp9.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : sam. 22 mars 2025 à 22:34
+-- Version du serveur : 5.7.24
+-- Version de PHP : 8.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `junia_tp9`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `classes`
+--
+
+CREATE TABLE `classes` (
+  `id` int(10) NOT NULL,
+  `libelle` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `classes`
+--
+
+INSERT INTO `classes` (`id`, `libelle`) VALUES
+(1, 'Terminal B'),
+(2, 'Seconde C'),
+(3, 'Terminal E'),
+(4, 'Premiere A');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etudiants`
+--
+
+CREATE TABLE `etudiants` (
+  `id` int(5) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `classe_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `etudiants`
+--
+
+INSERT INTO `etudiants` (`id`, `prenom`, `nom`, `classe_id`) VALUES
+(4, 'Albert', 'Schuman', 1),
+(5, 'Alphonse', 'Cabrol', 2),
+(6, 'Martin', 'Hurvois', 1),
+(7, 'Bastien', 'Le Corguillé', 3),
+(8, 'Elliott', 'Amrouche', 3),
+(9, 'Noah', 'Normand', 2),
+(13, 'Wilhelm', 'Macaigne', 1),
+(16, 'Einstein', 'Albert', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `matiere`
+--
+
+CREATE TABLE `matiere` (
+  `id` int(11) NOT NULL,
+  `lib` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `matiere`
+--
+
+INSERT INTO `matiere` (`id`, `lib`) VALUES
+(1, 'SQL'),
+(2, 'WEB'),
+(3, 'Sport'),
+(4, 'Danse'),
+(5, 'maths');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `professeurs`
+--
+
+CREATE TABLE `professeurs` (
+  `id` int(5) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `id_matiere` int(10) NOT NULL,
+  `id_classe` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `professeurs`
+--
+
+INSERT INTO `professeurs` (`id`, `prenom`, `nom`, `id_matiere`, `id_classe`) VALUES
+(4, 'Antoine', 'De Conto', 1, 2),
+(5, 'Jesus', 'Christ', 2, 2),
+(6, 'Dora', 'l\'exploratrice', 4, 1),
+(7, 'Ralph', 'Zogo', 3, 3);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `etudiants`
+--
+ALTER TABLE `etudiants`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_classe_student` (`classe_id`);
+
+--
+-- Index pour la table `matiere`
+--
+ALTER TABLE `matiere`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `professeurs`
+--
+ALTER TABLE `professeurs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_prof_matiere` (`id_matiere`),
+  ADD KEY `fk_prof_classe` (`id_classe`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `etudiants`
+--
+ALTER TABLE `etudiants`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT pour la table `matiere`
+--
+ALTER TABLE `matiere`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `professeurs`
+--
+ALTER TABLE `professeurs`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `etudiants`
+--
+ALTER TABLE `etudiants`
+  ADD CONSTRAINT `fk_classe_student` FOREIGN KEY (`classe_id`) REFERENCES `classes` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `professeurs`
+--
+ALTER TABLE `professeurs`
+  ADD CONSTRAINT `fk_prof_classe` FOREIGN KEY (`id_classe`) REFERENCES `classes` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_prof_matiere` FOREIGN KEY (`id_matiere`) REFERENCES `matiere` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
